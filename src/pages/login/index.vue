4f04fb350aadd45232a3a64beb825d0a008c78f9
xcwen <xcwen@qq.com>
2024-05-06 16:29:16 +0800

    新增注册页面并更新登录页面
    在登录页面进行了两个主要的更改：1.更改了登录按钮的尺寸。2.将"立即注册"文本链接更改为导航至新的注册页面的链接。此外，新建了注册页面。该页面包含用户注册需要的所有字段和验证规则，为用户提供直接的注册入口。同时，也在 pages.json 文件中添加了新页面的路径。

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -68,14 +68,14 @@
                         </block>
                         <button :loading="loginLoading" @click="mobileLogin" class="btn2-css3 submit_btn" :disabled="isloginDisabled">确 认</button>
                         <view class="rule-text">
-                            <tigCheckbox v-model:checked="is_checked" checked-color="#ee0a24"></tigCheckbox>
+                            <tigCheckbox v-model:checked="is_checked" :checkedSize="0.6" checked-color="#ee0a24"></tigCheckbox>
                             <view class="rule-xieyi">
                                 <text>登录即为同意</text>
                                 <text class="red" @chick="showAgreement">《商城用户服务协议》</text>
                                 <text class="red" @chick="showAgreement">《商城用户隐私协议》</text>
                             </view>
                         </view>
-                        <view class="register"> 没有账号？<text @chick="userRegistration" class="red">立即注册</text> </view>
+                        <view class="register"> 没有账号？<navigator url="/pages/register/index" class="red">立即注册</navigator> </view>
                     </view>
                 </view>
             </view>
