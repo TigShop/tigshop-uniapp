939f2655303bf4784c3b38fa5e02b371c6e2cb53
黄薇 <405538691@qq.com>
2024-05-10 17:03:07 +0800

    评论列表, 和上传图片组件

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,12 @@ import tabbar from "./components/tabbar/index.vue";
 import FormatPrice from "./components/format/price.vue";
 import tigpopup from "@/components/tigpopup/index.vue";
 import tigImage from '@/components/tigImage/index.vue'
+import tigUpload from '@/components/tigUpload/index.vue'
 
 export function createApp() {
     const app = createSSRApp(App);
     app.use(pinia);
-    app.component("tabbar", tabbar).component("FormatPrice", FormatPrice).component("tigpopup", tigpopup).component("tigImage", tigImage);
+    app.component("tabbar", tabbar).component("FormatPrice", FormatPrice).component("tigpopup", tigpopup).component("tigImage", tigImage).component("tigUpload", tigUpload);
     return {
         app,
         pinia
