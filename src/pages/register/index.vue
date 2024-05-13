bbe77d6b43225bc1fc8509c6dbbb954eb981f6ae
xcwen <xcwen@qq.com>
2024-05-13 16:26:30 +0800

    优化用户注册功能和界面
    针对用户注册功能进行了优化和改进。主要更新了用户信息输入表单、表单验证规则以及注册接口，确保用户输入信息的正确有效。同时，也增强了对用户界面的布局和设计，使得注册过程更为简单易懂。

--- src/pages/register/index.vue
+++ src/pages/register/index.vue
@@ -4,34 +4,57 @@
         <view class="register-warp">
             <view class="h1_tit">会员注册</view>
         </view>
-        <view class="form-con">
-            <uni-easyinput :inputBorder="false" :value="mobile" class="uni-input item-input" placeholder="请输入手机号" primaryColor="rgb(192, 196, 204)" @input="inputMobile"></uni-easyinput>
-        </view>
-        <view class="form-con fl-item">
-            <uni-easyinput :inputBorder="false" :value="mobileCode" class="uni-input item-input lf-input" focus placeholder="手机短信验证码" primaryColor="rgb(192, 196, 204)" @input="inputMobileCode" />
-            <VerificationCode
-                v-model:isChecked="is_checked"
-                v-model:mobile="mobile"
-                v-model:verify-token-data="verifyToken"
-                :requestApi="sendMobileCode"
-                @mobileErrorCallback="mobileErrorCallback"
-            ></VerificationCode>
-        </view>
-        <view class="form-con fl-item">
-            <uni-easyinput :inputBorder="false" :value="password" class="uni-input item-input" focus placeholder="密码" primaryColor="rgb(192, 196, 204)" type="password" @input="inputPassword" />
-        </view>
-        <view class="form-con fl-item">
-            <view class="lf-input  registration-agreement">
-                <tigCheckbox v-model:checked="is_checked" :checkedSize="0.6" checkedText="《注册协议》"></tigCheckbox>
+        <view class="profile-edit-content">
+            <uni-forms ref="formRef" :label-width="80" :modelValue="formState" :rules="formRules">
+                <uni-forms-item label="用户名" name="username" required>
+                    <view class=" input">
+                        <uni-easyinput :inputBorder="false" :value="formState.username" class="uni-input item-input" placeholder="请输入用户名" primaryColor="rgb(192, 196, 204)" @input="inputUsername"></uni-easyinput>
+                    </view>
+                </uni-forms-item>
+                <uni-forms-item label="手机号" name="mobile" required>
+                    <view class=" input">
+                        <uni-easyinput :inputBorder="false" :value="formState.mobile" class="uni-input item-input" placeholder="请输入手机号" primaryColor="rgb(192, 196, 204)" @input="inputMobile"></uni-easyinput>
+                    </view>
+                </uni-forms-item>
+                <uni-forms-item label="验证码" name="mobile_code" required>
+                    <view class="item-one ">
+                        <uni-easyinput :inputBorder="false" :value="formState.mobile_code" class="uni-input item-input input" focus placeholder="手机短信验证码" primaryColor="rgb(192, 196, 204)" @input="inputMobileCode" />
+                        <VerificationCode
+                            v-model:isChecked="is_checked"
+                            v-model:mobile="formState.mobile"
+                            v-model:verify-token-data="verifyToken"
+                            :requestApi="sendMobileCode"
+                            @mobileErrorCallback="mobileErrorCallback"
+                        ></VerificationCode>
+                    </view>
+                </uni-forms-item>
+                <uni-forms-item label="设置密码" name="password" required>
+                    <view class=" input">
+                        <uni-easyinput :inputBorder="false" :value="formState.password" class="uni-input item-input" placeholder="请输入密码" primaryColor="rgb(192, 196, 204)" @input="inputPassword"></uni-easyinput>
+                    </view>
+                </uni-forms-item>
+                <uni-forms-item label="确认密码" name="confirm_password" required>
+                    <view class="  input">
+                        <uni-easyinput :inputBorder="false" :value="formState.confirmPassword" class="uni-input item-input" placeholder="请再次输入密码" primaryColor="rgb(192, 196, 204)" @input="inputConfirmPassword"></uni-easyinput>
+                    </view>
+                </uni-forms-item>
+                <uni-forms-item label="" name="">
+                    <view class="btn">
+                        <view>
+                            <tigCheckbox v-model:checked="is_checked" :checkedSize="0.6" checkedText="《注册协议》"></tigCheckbox>
+                        </view>
+                        <view class="lf-input">
+                            <view>已有账号？</view>
+                            <navigator class="red" url="/pages/login/index">立即登录</navigator>
+                        </view>
+                    </view>
+                </uni-forms-item>
+            </uni-forms>
+            <view class="form-con">
+                <button :disabled="isRegisterDisabled" :loading="loginLoading" type="warn" @click="onRegister">注 册</button>
             </view>
-            <view class="lf-input fl-item">
-                <view>已有账号？</view>
-                <navigator class="red" url="/pages/login/index">立即登录</navigator>
-            </view>
         </view>
-        <view class="form-con">
-            <button :disabled="isRegisterDisabled" :loading="loginLoading" type="warn" @click="onRegister">注 册</button>
-        </view>
+
     </view>
 </template>
 
@@ -42,6 +65,7 @@ import { useUserStore } from "@/store/user";
 import { sendMobileCode } from "@/api/login/login";
 import VerificationCode from "@/pages/login/src/verificationCode.vue";
 import tigCheckbox from "@/components/tigCheckbox/index.vue";
+import { userRegist } from "@/api/user/regist";
 
 const userStore = useUserStore();
 
@@ -51,27 +75,106 @@ const parameter = reactive({
     title: "注册"
 });
 const inputMobile = (e: any) => {
-    mobile.value = e;
+    formState.value.mobile = e;
+};
+const inputUsername = (e: any) => {
+    formState.value.username = e;
 };
 const inputMobileCode = (e: any) => {
-    mobileCode.value = e;
+    formState.value.mobile_code = e;
 };
 const inputPassword = (e: any) => {
-    password.value = e;
+    formState.value.password = e;
+};
+const inputConfirmPassword = (e: any) => {
+    formState.value.confirmPassword = e;
 };
 
-const mobile = ref("");
-const mobileCode = ref("");
-const password = ref("");
 const is_checked = ref(false);
 const verifyToken = ref("");
 const loginLoading = ref(false);
 const isRegisterDisabled = computed(() => {
-    return !mobile.value || !mobileCode.value || !password.value || !verifyToken.value;
+    return !formState.value.mobile || !formState.value.mobile_code || !formState.value.password || !formState.value.confirmPassword || !verifyToken.value;
 });
 
-const onRegister = () => {
+const formState = ref({
+    username: "",
+    mobile_code: "",
+    mobile: "",
+    password: "",
+    confirmPassword: ""
+});
+const validateUsername = (rule: any, value: any, callback: any) => {
+    if (!value) {
+        return callback(new Error("用户名不能为空"));
+    } else if (value.length > 20 || value.length < 4) {
+        return callback(new Error("用户名应为4-20位字符"));
+    } else if (/^\d+$/.test(value)) {
+        return callback(new Error("用户名不能全为数字"));
+    } else if (!/^[\u4E00-\u9FA5a-zA-Z0-9_]+$/.test(value)) {
+        return callback(new Error("用户名格式错误,请输入正确的用户名"));
+    } else {
+        callback();
+    }
+};
+const validatePassword = (rule: any, value: any, callback: any) => {
+    if (!value) {
+        return callback(new Error("密码不能为空"));
+    } else if (value.length > 20 || value.length < 6) {
+        return callback(new Error("密码应为6-20位字符"));
+    } else if (/\s/.test(value)) {
+        return callback(new Error("密码中不允许有空格"));
+    } else if (/^\d+$/.test(value)) {
+        return callback(new Error("密码不能全为数字"));
+    } else if (!/^(?=.*[\d\W]).+$/.test(value)) {
+        return callback(new Error("密码不能全为字母"));
+    } else if (!/^(?=.*[a-zA-Z0-9]).+$/.test(value)) {
+        return callback(new Error("密码不能全为符号"));
+    } else if (!/^(?!([a-zA-Z0-9])\1*$).+$/.test(value)) {
+        return callback(new Error("密码不能全为相同字符或数字"));
+    } else {
+        callback();
+    }
+};
+const validatePassword2 = (rule: any, value: any, callback: any) => {
+    if (!value) {
+        return callback(new Error("请再次输入密码"));
+    } else if (value != formState.value.password) {
+        return callback(new Error("两次密码不一致"));
+    } else {
+        callback();
+    }
+};
+const formRules = reactive<any>({
+    username: [{ validator: validateUsername, trigger: "blur", required: true }],
+    password: [{ validator: validatePassword, trigger: "blur", required: true }],
+    confirm_password: [{ validator: validatePassword2, trigger: "change", required: true }]
+});
 
+const onRegister = async () => {
+    try {
+        loginLoading.value = true;
+        const result = await userRegist(formState.value);
+        uni.showToast({
+            title: "注册成功",
+            duration: 1500,
+            icon: "none"
+        });
+        setTimeout(() => {
+            // 一秒后执行这里的代码
+            uni.redirectTo({
+                url: "/pages/login/index"
+            });
+        }, 1000);
+    } catch (error: any) {
+        uni.showToast({
+            title: error.message,
+            duration: 1500,
+            icon: "none"
+        });
+    } finally {
+        loginLoading.value = false;
+    }
 };
 
 const mobileErrorCallback = (msg: string) => {
@@ -104,42 +207,41 @@ page {
 
     }
 
-    .form-con {
-        padding: 20rpx 40rpx;
-
-        .item-input {
-            border-bottom: 0.03125rem solid #eee;
-
-            font-size: 28rpx;
-            color: #252525;
-
-        }
-    }
-
-    .fl-item {
+    .profile-edit-content {
+        margin: 0 40rpx;
+        border-radius: 15rpx;
+        padding: 20rpx;
         display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        font-size: 0.8rem;
+        flex-direction: column;
+        gap: 16rpx;
 
-        .lf-input {
-            flex: 1;
-        }
-
-        .registration-agreement {
+        .item-one {
             display: flex;
-            align-items: center;
-            color: #999999;
+            justify-content: space-between;
+            flex-direction: row;
+        }
+
+        .input {
+            padding-bottom: 10rpx;
+            border-bottom: 1px solid #dfdfdf;
 
         }
 
+        .btn {
+            margin-left: -200rpx;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0 0 0 40rpx;
+            .lf-input {
+
+                display: flex;
+                gap: 20rpx;
+            }
+        }
     }
 }
 
-.submit_btn {
-
-}
 
 :deep(.is-input-border) {
     border: none; /* 首先清除所有边框 */
