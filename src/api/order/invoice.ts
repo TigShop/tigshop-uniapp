e41844374464db098be168e28b7725ed24680dc3
heYong <2896299792@qq.com>
2024-05-17 17:04:39 +0800

    fix & README

--- src/api/order/invoice.ts
+++ src/api/order/invoice.ts
@@ -4,7 +4,7 @@ import type { InvoiceResponse } from "@/types/order/invoice";
 // 获取增票资质详情 状态
 export const getInvoiceStatus = () => {
     return request<InvoiceResponse>({
-        url: "user/invoice/get_status/",
+        url: "user/invoice/get_status",
         method: "get"
     });
 };
